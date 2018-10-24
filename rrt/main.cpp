8cb9f3d3511fcd24ea0975bb399a2e1198c0d10f
Zhuoyang Du <zydu@zju.edu.cn>
2018-10-24 15:44:41 +0800

    Initial Commit

---
+++ rrt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rrt
+//
+//  Created by zhuoyang on 2018/10/24.
+//  Copyright © 2018年 zhuoyang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
